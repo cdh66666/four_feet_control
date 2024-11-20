9c0d3afb6d0b24f312d2fea46c747a8aebb35938
cdh <87925267+cdh66666@users.noreply.github.com>
2024-11-21 02:50:04 +0800

    成功将simulink中matlab function代码迁移只matlab工作区，使用.m代码文件可以共享工作区变量，并且使用变量控制仿真环境！

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
  # matlab四足机器人VMC控制器 - 2024年11月19日
 
 ![matlab仿真运行界面](image/README/matlab仿真运行界面)
+
+![VMC_Trot控制控制代码](image/README/VMC_Trot控制控制代码)
+
+![matlab代码控制](image/README/matlab代码控制)
--- matlab/startup.m
+++ matlab/startup.m
@@ -2,7 +2,19 @@ clear;
 clc;
  
 global T beta Tf Ts M g simulink_step force_threshold L W H l1 l2 l3 floor_x floor_y floor_h z_init h_init damping stiff friction_v friction_p VMC_PID_P VMC_PID_D hd Ktd K_z K_vx K_vy K_vz K_wx K_wy K_wz;
-
+global t S state_vars error_prev_matrix p_init_swing state_d stop_time;
+% 初始化上一次的误差值矩阵，初始化为全零矩阵，维度为 (4, 3)，对应四条腿，每个腿的误差是三维向量
+error_prev_matrix = zeros(4, 3);
+% 初始化每条摆动腿的起始位置坐标（初始化为对应维度的全零矩阵，后续在 t == 0 时赋值）
+p_init_swing = zeros(4, 3);
+% 初始化状态变量矩阵，包含速度和角度信息，维度为 (6, 1)，分别对应 [vx; vy; vz; roll; pitch; yaw]
+state_vars = zeros(6, 1);
+%期望四足运行状态,[vx; vy; vz; roll; pitch; yaw]
+state_d = zeros(6, 1);
+%摆动相运行时间t
+t=0;
+%状态机状态S
+S=-1;
 % 步态周期 T/s
 T = 1;
 % 摆动相占空比 beta
@@ -11,7 +23,8 @@ beta = 0.5;
 Tf = beta * T;
 % 支撑相周期 Ts
 Ts = T - Tf;
-
+%仿真停止时间
+stop_time = 3;
 % 机体质量，单位：kg
 M = 12;
 % 重力加速度
@@ -31,11 +44,11 @@ floor_x = 2;%地面长
 floor_y = 2;%地面宽
 floor_h= 0.05;%地面高
 % 一开始离地高度
-z_init = 0.4;
+z_init = 0.5;
 %腿部初始保持高度
 h_init = 0.35;
 damping = 10;%接触阻尼
-stiff =10000;%接触高度
+stiff =10000;
 friction_v = 0.3;%动摩擦系数
 friction_p =0.7;%静摩擦系数
 % VMC 力位混合控置 PD 参数
