a87aac6bb78c29e17dcf75f7725dad29ac824de5
cdh <87925267+cdh66666@users.noreply.github.com>
2024-11-25 10:29:48 +0800

    更新RL文件夹，文件夹里自带conda环境（包含gym,pytorch,python等环境），用vscode打开即可直接使用，无需配置环境！！！

---
+++ RL/main.py
@@ -0,0 +1,24 @@
+import gym
+
+from gym.utils.env_checker import check_env
+
+
+
+
+env = gym.make("LunarLander-v2",render_mode="human")
+env.action_space.seed(42)
+
+observation,info = env.reset(seed=42)
+
+
+
+for _ in range(1000):
+    observation,reward,terminated,truncated,info = env.step(env.action_space.sample())
+
+    if terminated or truncated:
+        observation,info = env.reset()
+
+ 
+
+env.close()
+
